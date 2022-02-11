0ed31238a5290f7fc0db91421bfd89a804f3ade2
deepcloudlabs <deepcloudlabs@gmail.com>
2022-02-11 14:49:56 +0300

    Create Customer.java

---
+++ core-banking/src/com/example/banking/domain/Customer.java
@@ -0,0 +1,75 @@
+package com.example.banking.domain;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+// Entity -> Identity
+public class Customer {
+	private final String identityNo;
+	private String fullname;
+	private final Map<String, Account> accounts = new HashMap<>();
+
+	public Customer(String identityNo, String fullname) {
+		Objects.requireNonNull(identityNo);
+		Objects.requireNonNull(fullname);
+		this.identityNo = identityNo;
+		this.fullname = fullname;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		Objects.requireNonNull(fullname);
+		this.fullname = fullname;
+	}
+
+	public String getIdentityNo() {
+		return identityNo;
+	}
+
+	public Collection<Account> getAccounts() {
+		return List.copyOf(accounts.values());
+	}
+
+	public Collection<Account> addAccount(
+			Account account){
+		Objects.requireNonNull(account);
+		accounts.put(account.getIban(), account);
+		return getAccounts();
+	}
+	public Collection<Account> dropAccount(
+			Account account){
+		Objects.requireNonNull(account);
+		return dropAccount(account.getIban());
+	}
+	public Collection<Account> dropAccount(
+			String iban){
+		Objects.requireNonNull(iban);
+		accounts.remove(iban);
+		return getAccounts();
+	}
+	public double getBalance() {
+		return getAccounts()
+				  .stream()
+				  .mapToDouble(Account::getBalance)
+				  .sum();
+	}
+	public long getNumberOfNotEmptyAccounts() {
+		return getAccounts()
+				   .stream()
+				   .mapToDouble(Account::getBalance)
+				   .filter( balance -> balance > 0.0)
+				   .count();
+	}
+	public Collection<Account> getEmptyAccounts() {
+		return getAccounts()
+				.stream()
+				.filter( acc -> acc.getBalance() == 0.0)
+				.toList();
+	}
+}
