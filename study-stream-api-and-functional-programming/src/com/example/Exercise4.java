5d29d324b46623388ab882b17390fc422435fd1d
deepcloudlabs <deepcloudlabs@gmail.com>
2022-02-15 13:17:47 +0300

    Create Exercise4.java

---
+++ study-stream-api-and-functional-programming/src/com/example/Exercise4.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		var meyveler = List.of(
+		   "kiraz", "kavun", "karpuz", "kivi",
+		   "şeftali", "muz", "elma", "armut",
+		   "çilek"
+		);
+		meyveler.stream()
+		        .filter(new Predicate<String>() {
+		        	// anonymous class
+					@Override
+					public boolean test(String meyve) {
+						return meyve.matches("^k[a-zşğüçöı]*z$");
+					}
+				})
+		        .forEach(new Consumer<String>() {
+                    // anonymous class
+					@Override
+					public void accept(String meyve) {
+						System.out.println(meyve);
+					}
+		        });
+		meyveler.stream()
+		        .filter(meyve -> meyve.matches("^k[a-zşğüçöı]*z$"))
+	            .forEach( System.out::println);
+	}
+
+}
