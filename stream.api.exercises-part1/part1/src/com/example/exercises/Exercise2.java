5f4880fc97c9ebed826d734d555f8c0e9ff1fc60
deepcloudlabs <deepcloudlabs@gmail.com>
2022-02-15 15:13:07 +0300

    Update Exercise2.java

--- stream.api.exercises-part1/part1/src/com/example/exercises/Exercise2.java
+++ stream.api.exercises-part1/part1/src/com/example/exercises/Exercise2.java
@@ -1,7 +1,13 @@
 package com.example.exercises;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
+import com.example.domain.City;
 
 /**
  * 
@@ -12,7 +18,28 @@ public class Exercise2 {
 	private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
 
 	public static void main(String[] args) {
-		// Find the most populated city of each continent
+		Function<ContinentCity,Integer> populationExtractor =
+			cc -> cc.city().getPopulation();	
+		var highPopulatedCityOfEachContinent =
+		countryDao.findAllCountries()
+		          .stream()
+		          .map( 
+		              country 
+		              -> 
+		              country.getCities()
+		                     .stream()
+		                     .map(city -> new ContinentCity(country.getContinent(),city))
+		                     .toList()
+		           )
+		           .flatMap(List::stream)
+		           .collect(
+		        		Collectors.groupingBy(ContinentCity::continent, Collectors.maxBy(Comparator.comparing(populationExtractor)))
+		        	);
+		highPopulatedCityOfEachContinent.forEach((continent,continentCity)->{
+			System.out.println(String.format("%16s: %s,%d",continent,continentCity.get().city().getName(),continentCity.get().city().getPopulation()));
+		});
 	}
 
 }
+
+record ContinentCity(String continent, City city) {}
